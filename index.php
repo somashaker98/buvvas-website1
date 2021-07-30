3ab97f0feb5fd51c2b0c373066d4d22c2ff80add
srinivasWSA <srinivas@wesecureapp.com>
2021-07-30 10:52:18 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Buvvas</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css/bootstrap.css">
+<link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+
+<!-- Navbar start -->
+<div class="topnav">
+    <div class="logo"><img class="nav_logo" src="./image/logo.png" alt=""></div>
+    <div class="nav_links">
+        <a href="#">Company</a>
+        <a href="#">Services</a>
+        <a href="#">Products</a>
+        <a href="#">Support</a>
+    </div>
+    <!-- <div><button class= "sign_in btn btn-primary">Sign In</button></div> -->
+    
+</div>
+<!-- Navbar end -->
+
+<!-- Main Content start-->
+<div class="row">
+
+<!-- Column-1 start -->
+  <div class="column_1">
+    <h2>Cloud <br> Business Applications</h2>
+    <h5>Connecting Customers to Business</h5>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde dicta quibusdam, quae accusamus molestiae totam dolorum aperiam nulla error aliquid saepe neque ducimus fuga illum, numquam, aliquam qui mollitia porro.</p>
+    <button class="btn btn-primary">More Info</button>
+  </div>
+<!-- Column-1 end -->  
+
+<!-- Column-2 start -->
+  <div class="column_2">
+   
+  </div>
+<!-- Column-2 end -->  
+
+<!-- Column-3 start -->  
+  <div class="column_3">
+    <div id=slideset1>
+        <div class="web_dev">
+          <h2>Website Development</h2>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+          <button class="btn btn-primary">More Info</button>
+        </div>
+        <div class="mobile_app">
+          <h2>Mobile APP Development</h2>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+          <button class="btn btn-primary">More Info</button>
+        </div>
+        <div class="digital">
+          <h2>Digital Marketing</h2>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+          <button class="btn btn-primary">More Info</button>
+        </div>
+        <!-- <div>
+            <h2>Graphic Design</h2>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+        </div>
+        <div>
+            <h2>Content Writing</h2>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+        </div>
+        <div>
+            <h2>SEO writing</h2>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+        </div> -->
+      </div>
+     </div>
+<!-- Column-3 end -->  
+</div>
+<!-- Main Content end-->
+
+<footer class="footer-area position-relative">
+        <div class="container">
+        <div class="row justify-content-center footer-l1-area-items">
+          <div class="col-xl-2 col-lg-2 col-md-3 col-xs-4 col-6 order-2 order-lg-1">
+            <h3>Company</h3>
+            <ul class="list-unstyled">
+              <li><a href="#">About us</a></li>
+              <li><a href="#">Careers</a></li>
+              <li><a href="#">Contact us</a></li>
+              <li><a href="#">Testimonials</a></li>
+            </ul>
+          </div>
+          <div class="col-xl-2 col-lg-2 col-md-3 col-xs-4 col-6 order-3 order-lg-2">
+            <h3>Products</h3>
+            <ul class="list-unstyled">
+              <li><a href="#">Restaurant Billing Software</a></li>
+              <li><a href="#">Printers</a></li>
+              <li><a href="#">Android Devices</a></li>
+              <li><a href="#">Paper Rolls</a></li>
+              <li><a href="#">Swipe Machines</a></li>
+            </ul>
+          </div>
+          <div class="col-xl-3 col-lg-3 col-md-3 col-xs-4 col-12 order-4 order-lg-3">
+            <h3>Services</h3>
+            <ul class="list-unstyled ">
+              <li><a href="#">Digital Marketing</a></li>
+              <li><a href="#">Content Writing</a></li>
+              <li><a href="#">SEO for Business</a></li>
+              <li><a href="#">Web Development</a></li>
+              <li><a href="#">Graphic Design</a></li>
+            </ul>
+          </div>
+          <div class="offset-xl-1 col-xl-4 col-lg-5 col-md-10 mb-9 col-sm-12 order-1 order-lg-4">
+            <h3>Newsletter</h3>
+            <div class="bg-catskill subscribe-newsletter">
+              <h4>Get monthly updates on your inbox</h4>
+              <form action="#" class="d-sm-flex">
+                <input type="email" class="form-control" id="email-2" name="email" placeholder="Enter your email">
+                <button class="btn btn-style-02">Subscribe</button>
+              </form>
+              <p>We won’t spam you at all!</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+</footer>
+
+</body>
+</html>
